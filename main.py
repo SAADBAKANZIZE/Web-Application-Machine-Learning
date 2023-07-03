48e1d822c5004760b8f778c1c31eb003638c7b66
SAADBAKANZIZE <baknzizesaad@gmail.com>
2023-07-03 12:29:34 +0100

    Second  commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ THIS APP PREDICT THE TYPE OF THE IRIS FLOWER
 st.sidebar.header("INPUT PARAMETERS")
 
 def user_input():
-    sepal_length=st.sidebar.slider('The length Sepal',4.3,7.9,5.3)
+    sepal_length=st.sidebar.slider('The length of Sepal',4.3,7.9,5.3)
     sepal_width=st.sidebar.slider('The width of Sepal',2.0,4.4,3.3)
     petal_length=st.sidebar.slider('The length of Petal',1.0,6.9,2.3)
     petal_width=st.sidebar.slider('The width of Petal',0.1,2.5,1.3)
